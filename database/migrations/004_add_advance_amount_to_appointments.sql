a64b15f9778931eaca6776e578d33360c4491e38
Myurr11 <myurjoshi00742@gmail.com>
2026-03-14 22:24:46 +0530

    Remove database migration files

--- database/migrations/004_add_advance_amount_to_appointments.sql
+++
@@ -1,6 +0,0 @@
--- Add advance_amount column to appointments table
-ALTER TABLE appointments 
-ADD COLUMN IF NOT EXISTS advance_amount DECIMAL(10, 2) DEFAULT 0;
-
--- Add index for better query performance
-CREATE INDEX IF NOT EXISTS idx_appointments_advance_amount ON appointments(advance_amount);
--- database/migrations/005_add_description_to_services.sql
+++
@@ -1,3 +0,0 @@
--- Add description column to services table
-ALTER TABLE services 
-ADD COLUMN IF NOT EXISTS description TEXT;
